874f04c244622c26b76a3b9719c7b0aee4ff03ca
syoshi20 <sebastianyoshitsuru@gmail.com>
2023-01-06 22:57:34 -0300

    experinecias

--- portafolio-FrontEnd/src/app/app.module.ts
+++ portafolio-FrontEnd/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciasComponent
   ],
   imports: [
     BrowserModule,
