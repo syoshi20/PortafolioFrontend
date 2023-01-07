e53d1a5137a8b7135cf7a08ac45e296c18e58004
syoshi20 <sebastianyoshitsuru@gmail.com>
2023-01-07 03:34:12 -0300

    skill y educacion

--- portafolio-FrontEnd/src/app/app.module.ts
+++ portafolio-FrontEnd/src/app/app.module.ts
@@ -6,17 +6,40 @@ import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillComponent } from './components/skill/skill.component';
+
+import{ NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AcercaDeComponent,
-    ExperienciasComponent
+    ExperienciasComponent,
+    EducacionComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 60,
+      "space": -10,
+      "outerStrokeGradient": true,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#1d66b4",
+      "outerStrokeGradientStopColor": "#7abdff",
+      "innerStrokeColor": "#e7e8ea",
+      "innerStrokeWidth": 10,
+      "title": "UI",
+      "animateTitle": false,
+      "animationDuration": 1000,
+      "showUnits": false,
+      "showBackground": false,
+      "clockwise": false,
+      "startFromZero": false,
+      "lazy": true})
   ],
   providers: [],
   bootstrap: [AppComponent]
